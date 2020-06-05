637a53b919aa92fe931b746a6420cccec252df70
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-06-04 20:19:59 -0700

    Tooltip Visibility on Focus
    Elements with a tooltip present only showed the tooltip when hovered upon, now it appears when the element is focused as well!

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const cacheName = 1;
+
+self.addEventListener("activate",function(event){
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch",function(event){
+  event.respondWith(
+    fetch(event.request).then((resolve) => {
+      const resClone = resolve.clone();
+      caches.open(cacheName).then((cache) => {
+        cache.put(event.request, resClone);
+      });
+      return resolve;
+    })
+    .catch((err) => caches.match(event.request).then((resolve) => resolve))
+  );
+});
