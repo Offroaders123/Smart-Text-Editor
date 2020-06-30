9e6c6233ea1490d83fd0998de19a9e0e9bf169b1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-06-30 12:39:16 -0700

    Install Button Test Removal
    I may look into this again in the future, but having a dedicated button didn't seem very useful when the browser chrome has it's own dedicated button to install the PWA, so I reverted the source to how it wa before and made a few changes to the service worker that I've been meaning to do for a while now.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const cacheName = 1.35;
+const cacheName = 1.36;
 self.addEventListener("activate",function(event){
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(function(cacheNames){
       return Promise.all(
-        cacheNames.map((cache) => {
+        cacheNames.map(function(cache){
           if (cache !== cacheName) {
             return caches.delete(cache);
           }
@@ -14,13 +14,17 @@ self.addEventListener("activate",function(event){
 });
 self.addEventListener("fetch",function(event){
   event.respondWith(
-    fetch(event.request).then((resolve) => {
+    fetch(event.request).then(function(resolve){
       const resClone = resolve.clone();
-      caches.open(cacheName).then((cache) => {
-        cache.put(event.request, resClone);
+      caches.open(cacheName).then(function(cache){
+        cache.put(event.request,resClone);
       });
       return resolve;
     })
-    .catch((err) => caches.match(event.request).then((resolve) => resolve))
+    .catch(function(error){
+      caches.match(event.request).then(function(resolve){
+        return resolve;
+      })
+    })
   );
 });
