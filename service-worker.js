b8cd11b502c3031ed3234e4c7515c418f5ee5ab3
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-08-28 21:49:11 -0700

    Google Search Index + Service Worker Formatting
    This commit should allow Smart Text Editor to be indexed for Google searches! Formatting changes were made to the service worker as well, let's hope it will work!

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,21 @@
-const cacheName = 1.66;
+var cacheName = 1.67;
 self.addEventListener("activate",function(event){
-  event.waitUntil(
-    caches.keys().then(function(cacheNames){
-      return Promise.all(
-        cacheNames.map(function(cache){
-          if (cache !== cacheName) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(function(cacheNames){
+    return Promise.all(cacheNames.map(function(cache){
+      if (cache != cacheName){
+        return caches.delete(cache);
+      }
+    }));
+  }));
 });
 self.addEventListener("fetch",function(event){
-  event.respondWith(
-    fetch(event.request).then(function(resolve){
-      const resClone = resolve.clone();
-      caches.open(cacheName).then(function(cache){
-        cache.put(event.request,resClone);
-      });
+  event.respondWith(fetch(event.request).then(function(resolve){
+    caches.open(cacheName).then(function(cache){
+      cache.put(event.request,resolve.clone);
+    });
+    return resolve;
+    }).catch(function(error){
+    caches.match(event.request).then(function(resolve){
       return resolve;
-    })
-    .catch(function(error){
-      caches.match(event.request).then(function(resolve){
-        return resolve;
-      })
-    })
-  );
+  })}));
 });
