9979be04dd56186dc9e2ec96640d2a4257eea34d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-09-15 23:57:17 -0700

    New Window Rewrite + Semantic Changes
    Rewrote the actions that are made when opening new editor and display windows. Semantic changes were made to the markup for some elements as well.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 1.82;
+var cacheVersion = 1.83;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
