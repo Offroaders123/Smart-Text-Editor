d077f0d205509f6af0cce0f9274d0515d7d6e5a1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-09-21 15:50:48 -0700

    Card Focus Changes + If Statement Simplifications

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 1.88;
+var cacheVersion = 1.89;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
