6df18112c711d34c620a50043db53c51ff15925f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-09-27 16:10:22 -0700

    Multi-File Editing + Improved Mobile Support + Online Resources Icons
    Major features:
    - Multiple files can now be edited at once, and each can be opened by clicking the editor's associated tab.
    - When using devices with notches and camera cutouts, the site should now expand to the edges and leave space for protruding cameras or other hardware accordingly.
    - Starting the site in standalone mode on iOS now shows a custom startup screen with the Smart Text Editor logo.
    - Links to online resources from within the Tools dropdown now have dedicated vector icons.
    
    Other notable changes and fixes:
    - Modals now have a max height, they no longer extend out of the top and bottom of some small screens or browser windows.
    - Drag and drop now works with all textareas throughout the site, not just the active editor.
    - Fixed line height of text within textareas to no longer overlap with other lines when in Text view.
    - Widget textarea inset box shadows now display above overflowing content.
    - Card navigation buttons now have ARIA labels for screen readers.
    - Compacted JavaScript functions and if statements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 1.89;
+var cacheVersion = 1.90;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
