9fa173297ce9eb99faac17c8d3941ef5f0c9c1b7
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-10-06 14:50:27 -0700

    File Tabs Restyling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 1.96;
+var cacheVersion = 1.97;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
