6bfbc27c0a519855a1d6bc7c6fdbee8373e30a75
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-10-06 17:07:45 -0700

    File Tabs Rename Button
    This update adds a simple implementation to rename files currently open in an editor. This does not rename the file on the user's drive, it only changes the name to be different when saved to their drive than when they first opened it in an editor.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 1.97;
+var cacheVersion = 1.98;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
