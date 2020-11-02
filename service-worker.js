5572d57100af2416cbf673a6b79c8777ec54e7c1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-11-01 21:07:22 -0800

    Minimizeable Widgets + Responsive Split Scaling + Editing Tools Setting Changes
    Changes:
    - Widgets can now be minimized! They are shrunk down to provide more room for the workspace, while still being easily accessible. This can be done by pressing the minimize button of the active widget, or by pressing Ctrl + Shift + M / Shift + Cmd + M.
    - Editing Tools and widgets no longer overlap with the files tab section, and they move up accordingly as space is provided or taken away. This is most visible in Split view in Vertical when scaling the workspace scaler to overlap with the current widget, or Editing Tools.
    - Editing Tools no longer has the option to open automatically for touch devices, and the new default option is for Editing Tools is to be closed on startup. It can still be enabled to appear on startup all of the time, no matter the type of device the user is on.
    - The last element that had focus before a modal is opened is now re-focused when all modals are closed.
    - Commonly used variables within the editor are now within a global "editor" object, rather than being defined as individual variables.
    
    Fixes:
    - Opening files while using Chrome versions that only support the Native File System API flag implementation now open successfully again.
    - The default drag and drop implementation for text is now used by all textareas and text inputs. This means that undo and redo actions are preserved after text is dragged in.
    - Various element box shadows now scale with the viewport size.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheVersion = 2.06;
+var cacheVersion = 2.07;
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
     return Promise.all(cacheVersions.map(function(cache){
