ddcb35759bd55f66bfb6584de091ba8bb546ca74
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-11-13 04:19:10 -0800

    Web Sharing APIs + More Context Menus + Site Sharing Images
    Changes:
    - Devices that support the Web Sharing and Web Share Target APIs can now take advantage of these APIs with text, link addresses, and importing files using the system sharing interface. Files cannot yet be shared from the editor to other apps with the system sharing interface, this will come in a future update.
    - Added context menus for anchor tags, and added a dropdown menu to the Settings button within the header that will open with the same input as openign a context menu (right click for mouse, long press for touch).
    - Added Open Graph sharing images for social media and other methods that use the API.
    - Saving a file will now automatically update the preview window.
    - Dropdowns and context menus will now close automatically when opening or closing modals.
    - Simplified settings modal layout.
    
    Fixes:
    - Fixed detection of whether the shift key is pressed during keyboard shortcut activations.
    - The "getNavigableElements" function was having issues with sorting out navigable elements that were not displayed, and this is now fixed.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var worker = {
-  cache_version: 2.13
+  cache_version: 2.14
 }
 self.addEventListener("activate",function(event){
   event.waitUntil(caches.keys().then(function(cacheVersions){
@@ -9,6 +9,18 @@ self.addEventListener("activate",function(event){
   }));
 });
 self.addEventListener("fetch",function(event){
+  if (event.request.method == "POST"){
+    return event.respondWith(async function(){
+      var client = await self.clients.get(event.clientId || event.resultingClientId), requestData = await event.request.formData(), files = requestData.getAll("media");
+      files.forEach(function(mediaFile,index){
+        files[index] = mediaFile.get("file");
+      });
+      client.postMessage({
+        action: "load-files",
+        files: files
+      });
+    });
+  }
   event.respondWith(fetch(event.request).then(function(resolve){
     var resolveClone = resolve.clone();
     caches.open(worker.cache_version).then(function(cache){
@@ -20,14 +32,16 @@ self.addEventListener("fetch",function(event){
   }));
 });
 self.addEventListener("message",function(event){
-  if (event.data == "clear-cache"){
+  if (event.data.action == "clear-cache"){
     caches.keys().then(function(cacheVersions){
       Promise.all(cacheVersions.map(function(cache){
         return caches.delete(cache);
       }));
       self.clients.matchAll().then(function(clients){
         clients.forEach(function(client){
-          client.postMessage("clear-cache-success");
+          client.postMessage({
+            action: "clear-cache-success"
+          });
         });
       });
     });
