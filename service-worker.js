3d61e3ba775be075b461b45a44b8cd9976180c01
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-12-13 04:16:07 -0800

    Wider File Extension Support + Split View Scaler Improvements + Insane Function Compression
    Changes:
    - Removed restrictions for specific file types that can be opened with the editor from the file system, as this is going to be managed within the editor's settings in the future. This will eventually allow the user to open any file they like, no matter if the editor has declared support for it. This does allow the user to be able to open files that should not be parsed as text, but the UI will ask them if they would like to still import, if it is not a yet-known file extension.
    - Added .php, .yaml, .yml, .dist, .ini, .markdown, and .properties as preapproved file extensions.
    - Adjusted scrollbar sizes and margins.
    - Capitalized the global Editor object (previously "editor").
    - Moved multiple functions that query information about open editors into the global Editor object.
    - Changed most functions to use the ES6 arrow function syntax instead of the standard function(){} syntax. This produced a great unintended effect of shrinking the site's main source code by 123 lines! (Don't forget that this update added some more as well 😮)
    
    Fixes:
    - Split View scaler movement detection is now relative to the pointer's position on the page, rather than the viewport.
    - Adjusted dropdown submenu appearance and behavior to work better when the site is within a small viewport.
    - Prevented the body from having unintended overflow when navigating the page with the Page Up and Page Down keys.
    - Increased the padding size of the Split View scaler handle.

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,34 @@
 var worker = {
-  cache_version: 2.16
+  cache_version: 2.17
 }
-self.addEventListener("activate",function(event){
-  event.waitUntil(caches.keys().then(function(cacheVersions){
-    return Promise.all(cacheVersions.map(function(cache){
-      if (cache != worker.cache_version) return caches.delete(cache);
-    }));
-  }));
+self.addEventListener("activate",event => {
+  event.waitUntil(caches.keys().then(cacheVersions => Promise.all(cacheVersions.map(cache => {
+    if (cache != worker.cache_version) return caches.delete(cache);
+  }))));
 });
-self.addEventListener("fetch",function(event){
+self.addEventListener("fetch",event => {
   if (event.request.method == "POST"){
-    return event.respondWith(async function(){
+    event.respondWith(async () => {
       var client = await self.clients.get(event.clientId || event.resultingClientId), requestData = await event.request.formData(), files = requestData.getAll("media");
-      files.forEach(function(mediaFile,index){
-        files[index] = mediaFile.get("file");
-      });
+      files.forEach((mediaFile,index) => files[index] = mediaFile.get("file"));
       client.postMessage({
         action: "load-files",
         files: files
       });
     });
-  }
-  event.respondWith(fetch(event.request).then(function(resolve){
+  } else event.respondWith(fetch(event.request).then(resolve => {
     var resolveClone = resolve.clone();
-    caches.open(worker.cache_version).then(function(cache){
-      cache.put(event.request,resolveClone);
-    });
+    caches.open(worker.cache_version).then(cache => cache.put(event.request,resolveClone));
     return resolve;
-  }).catch(function(error){
-    return caches.match(event.request);
-  }));
+  }).catch(error => caches.match(event.request)));
 });
-self.addEventListener("message",function(event){
+self.addEventListener("message",event => {
   if (event.data.action == "clear-cache"){
-    caches.keys().then(function(cacheVersions){
-      Promise.all(cacheVersions.map(function(cache){
-        return caches.delete(cache);
-      }));
-      self.clients.matchAll().then(function(clients){
-        clients.forEach(function(client){
-          client.postMessage({
-            action: "clear-cache-success"
-          });
-        });
-      });
+    caches.keys().then(cacheVersions => {
+      Promise.all(cacheVersions.map(cache => caches.delete(cache)));
+      self.clients.matchAll().then(clients => clients.forEach(client => client.postMessage({
+        action: "clear-cache-success"
+      })));
     });
   }
 });
