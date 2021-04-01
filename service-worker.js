12e7b3110153b19083d9b2b2d8363444a8321e94
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-04-01 12:57:53 -0700

    SW Powered Responsive App Manifest
    The App Manifest's responsiveness in regards to site origin and platform-specific icons are now managed by the service worker! This allows the modified manifest to be cacheable and to be modified before the default manifest is sent to the client.
    
    Another change to note: The site is no longer installable before the service worker has had a chance to load. The site is only installable after the service worker has provided the complete App Manifest for the client to read.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,29 @@
-self.worker = {
-  version: 2.22
+self.Editor = {
+  version: 2.27,
+  origin: "https://offroaders123.github.io/Smart-Text-Editor",
+  environment: () => ({
+    macOS_device: (/(Mac)/i.test(navigator.platform) && navigator.standalone == undefined)
+  })
 }
 self.addEventListener("activate",event => {
   event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
-    if (cache != worker.version) return caches.delete(cache);
+    if (cache != Editor.version) return caches.delete(cache);
   }))));
 });
 self.addEventListener("fetch",event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).then(response => caches.open(worker.version).then(cache => {
+  if (event.request.url == `${Editor.origin}/manifest.webmanifest`){
+    return event.respondWith(fetch("manifest.webmanifest").then(async request => {
+      var manifest = await request.json();
+      manifest.icons = manifest.icons.filter(icon => {
+        if (!Editor.environment().macOS_device && icon.platform != "macOS") return icon;
+        if (Editor.environment().macOS_device && icon.platform == "macOS" || icon.purpose == "maskable") return icon;
+      });
+      var response = new Response(new Blob([JSON.stringify(manifest,null,"  ").replace(/_origin_/g,Editor.origin)],{ type: "text/json" }));
+      caches.open(Editor.version).then(cache => cache.put(event.request,response));
+      return response.clone();
+    }));
+  }
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).then(response => caches.open(Editor.version).then(cache => {
     cache.put(event.request,response.clone());
     return response;
   }))));
