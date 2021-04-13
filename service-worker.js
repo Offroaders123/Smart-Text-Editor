8e60fd15f455b2e713593779a2889ff1b4cd9d97
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-04-13 11:35:50 -0700

    Num-Text Web Components + Removed Electron Functionality
    Changes:
    - Updated all numbered textareas to the newly designed <num-text> elements from my Numbered-Textarea-Plugin project.
    - Removed all Electron-specific features, as I don't have plans to make an Electron version of the app anymore.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.Editor = {
-  version: 2.29,
+  version: 2.30,
   origin: "https://offroaders123.github.io/Smart-Text-Editor",
   environment: () => ({
     macOS_device: (/(Mac)/i.test(navigator.platform) && navigator.standalone == undefined)
