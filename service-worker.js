edfab07ebd0fd7786bf744fb88a33eb81bbff811
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-08-14 11:48:20 -0700

    Stricter Variable Interactions + Web Share Target Removal
    Thanks to Vio Cassel for the Pull Request! He suggested strengthening some of the variable definitions and type checking within the Service Worker. I haven't looked too closely into those areas of JS yet, I think it would be a nice improvement to my scripts overall to start using those. Once I know how they work a little more, I'll go through the rest of the project to update any other places that could use the same help.
    
    Here's a link to the Pull Request for the parts of this commit suggested by Vio Cassel:
    https://github.com/Offroaders123/Smart-Text-Editor/pull/8
    
    I also removed the Web Share Target information for now, I may re-add it later when I have a change to be able to test it on devices that support it.

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
 self.Editor = {
-  version: 2.88,
+  version: 2.89,
   environment: () => ({
-    macOS_device: (/(Mac)/i.test(navigator.platform) && navigator.standalone == undefined)
+    macOS_device: (/(Mac)/i.test(navigator.platform) && navigator.standalone === undefined)
   })
 }
 self.addEventListener("activate",event => {
   event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
-    if (cache != Editor.version) return caches.delete(cache);
+    if (cache !== Editor.version) return caches.delete(cache);
   }))));
   event.waitUntil(clients.claim());
   postMessageAllClients({ action: "service-worker-activated" });
 });
 self.addEventListener("fetch",event => {
-  if (event.request.url == `${self.location.href.match("(.*\/).*")[1]}manifest.webmanifest`){
+  if (event.request.url === `${self.location.href.match("(.*\/).*")[1]}manifest.webmanifest`){
     return event.respondWith(caches.match(event.request).then(response => {
       return response || fetch("manifest.webmanifest").then(async request => {
-        var manifest = await request.json();
+        const manifest = await request.json();
         manifest.icons = manifest.icons.filter(icon => {
-          if (!Editor.environment().macOS_device && icon.platform != "macOS") return icon;
-          if (Editor.environment().macOS_device && icon.platform == "macOS" || icon.purpose == "maskable") return icon;
+          if (!Editor.environment().macOS_device && icon.platform !== "macOS") return icon;
+          if (Editor.environment().macOS_device && icon.platform === "macOS" || icon.purpose === "maskable") return icon;
         });
-        var response = new Response(new Blob([JSON.stringify(manifest,null,"  ")],{ type: "text/json" }));
+        const response = new Response(new Blob([JSON.stringify(manifest,null,"  ")],{ type: "text/json" }));
         caches.open(Editor.version).then(cache => cache.put(event.request,response));
         return response.clone();
       });
@@ -34,7 +34,7 @@ self.addEventListener("fetch",event => {
   }));
 });
 self.addEventListener("message",event => {
-  if (event.data.action == "clear-site-caches"){
+  if (event.data.action === "clear-site-caches"){
     caches.keys().then(versions => {
       Promise.all(versions.map(cache => caches.delete(cache)));
       postMessageAllClients({ action: "clear-site-caches-complete" });
@@ -44,18 +44,3 @@ self.addEventListener("message",event => {
 function postMessageAllClients(data){
   clients.matchAll().then(clients => clients.forEach(client => client.postMessage(data)));
 }
-
-/* Web Share Target hook - to be re-added to the fetch event at some point
-
-  if (event.request.method == "POST"){
-    event.respondWith(async () => {
-      var client = await self.clients.get(event.clientId || event.resultingClientId), requestData = await event.request.formData(), files = requestData.getAll("media");
-      files.forEach((mediaFile,index) => files[index] = mediaFile.get("file"));
-      client.postMessage({
-        action: "load-files",
-        files: files
-      });
-    });
-  } else 
-
-*/
