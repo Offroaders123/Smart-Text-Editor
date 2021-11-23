b328f65b45933bd9b66a40fe65e242eb9d8f9b7f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-11-22 21:29:28 -0800

    Preview Base URL + Preview Options
    Additions:
    - Added a new way to automatically set the Base URL for preview window sources! Now you can have a specific Base URL for your STE preview, and a separate one for the actual source of your HTML. This is helpful when running a live server of your project in a terminal and viewing on another device at the same time. Now you can set a Base URL override for inside your STE instance only, and any other devices connecting to the server can have the original Base URL destination.
    - Added the Refresh keyboard shortcut as a clickable option inside of the Preview dropdown. There were already shortcuts to activate it, but now they will also show on the option, as they weren't findable anywhere in the app before. Now it should be easier for people to know about it.
    
    Changes:
    - Shrunk the header text size for dialogs. It seemed like the original size was a little too big while making the new Base URL card.
    
    Fixes:
    - Fixed an issue in iOS Safari where the page's theme color would show when "scrolling" the page with your finger, even though the <body> is fixed to the viewport. In other words, the <body> background will still scroll with the movement of your finger, even if the <body> itself isn't scrolling anywhere. I took care of this by making the <html> and <body> tags have the same background color, so they will each cover each other up when the weird spot used to show through.
    I will look into making a WebKit bug report for this soon.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.Editor = {
-  version: 3.06,
+  version: 3.10,
   cache: true,
   environment: () => ({
     macOS_device: (/(macOS|Mac)/i.test(("userAgentData" in navigator) ? navigator.userAgentData.platform : navigator.platform) && navigator.standalone === undefined)
