e6190112eda67885020811a922f4b6705abe320b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-01-12 22:53:29 -0800

    WCO Geometry Change Detection
    Temprarily improved the current system I had for detecting changes in
    the Window Controls Overlay state. A lot of the app feels like a lot of
    spaghetti code as of now, so this will be a fix until much nicer
    detection is put in place. I had it more difficult than it needed to be
    previously. There is a dedicated `geometrychange` event under the
    `navigator.windowControlsOverlay` property, and this is much more
    reliable than simply detecting resize events on the window, which is
    what I was using before, for some reason.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.Editor = {
-  version: 3.13,
+  version: 3.14,
   cache: true,
   environment: () => ({
     macOS_device: (/(macOS|Mac)/i.test(("userAgentData" in navigator) ? navigator.userAgentData.platform : navigator.platform) && navigator.standalone === undefined)
