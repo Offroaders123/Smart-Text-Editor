50d76a3e5857457db071f1f3c236dec9b4ca6427
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2026-01-07 19:43:43 -0800

    Preview Refresh Fixed!
    The unsaved dialog is broken though. It seems like it always shows, even if you didn't type anything. Just clicking on the window seems to enable it. What the heck? Note that it will not show in dev mode. I had to find that out after looking at my code again. I still love Nonkertompf.

--- src/workspace/Editor.tsx
+++ src/workspace/Editor.tsx
@@ -1,6 +1,7 @@
 import { createEffect, onMount } from "solid-js";
-import { editorRef, editorValue, setEditorRef } from "../app.js";
+import { editorRef, editorRefresh, editorUnsaved, editorValue, setEditorRef, setEditorRefresh, setEditorUnsaved, setEditorValue } from "../app.js";
 import { applyEditingBehavior } from "../dom.js";
+import { refreshPreview } from "./Workspace.js";
 import "./Editor.scss";
 
 export function Editor() {
@@ -21,6 +22,17 @@ export function Editor() {
     <textarea
       class="NumText Editor"
       ref={ref!}
+      value={editorValue()}
+      oninput={event => {
+        setEditorValue(event.currentTarget.value);
+        if (!editorRefresh()){
+          setEditorRefresh(true);
+        }
+        if (!editorUnsaved()){
+          setEditorUnsaved(true);
+        }
+        refreshPreview();
+      }}
     />
   );
 }
